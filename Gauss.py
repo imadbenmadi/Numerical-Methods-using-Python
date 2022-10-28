558b620aaabe1bd86732be6cd76914ca444457da
imadbenmadi <106430872+imadbenmadi@users.noreply.github.com>
2022-10-28 16:51:49 +0100

    Update Gauss.py

--- Gauss.py
+++ Gauss.py
@@ -1,45 +1,47 @@
 import numpy as np
 import sys
+def Gauss():
 
-n = int(input('Enter number of unknowns: '))
+    n = int(input('Enter number of unknowns: '))
 
-a = np.zeros((n,n+1))
+    a = np.zeros((n,n+1))
 
-x = np.zeros(n)
+    x = np.zeros(n)
 
-print('Enter the Matrix Coefficients:')
-for i in range(n):
-    for j in range(n+1):
-        a[i][j] = float(input( 'a['+str(i)+']['+ str(j)+']='))
+    print('Enter the Matrix Coefficients:')
+    for i in range(n):
+        for j in range(n+1):
+            a[i][j] = float(input( 'a['+str(i)+']['+ str(j)+']='))
 
-print(a)
-#Gauss Elimination
-for i in range(n):
-    if a[i][i] == 0.0:
-        sys.exit('Eurore : can not divide by zero!')
-        
-    for j in range(i+1, n):
-        ratio = a[j][i]/a[i][i]
-        
-        for k in range(n+1):
+    print(a)
+    #Gauss Elimination
+    for i in range(n):
+        if a[i][i] == 0.0:
+            sys.exit('Eurore : can not divide by zero!')
             
-            a[j][k] = a[j][k] - ratio * a[i][k]
+        for j in range(i+1, n):
+            ratio = a[j][i]/a[i][i]
             
-print("the matrix after elimination : ")
-print(a)
+            for k in range(n+1):
+                
+                a[j][k] = a[j][k] - ratio * a[i][k]
+                
+    print("the matrix after elimination : ")
+    print(a)
 
 
-x[n-1] = a[n-1][n]/a[n-1][n-1]# this is the last solution : xn
+    x[n-1] = a[n-1][n]/a[n-1][n-1]# this is the last solution : xn
 
-for i in range(n-2,-1,-1):
-    x[i] = a[i][n]
-    for j in range(i+1,n):
-        print(j)
-        x[i] = x[i] - a[i][j]*x[j]
-    
-    x[i] = x[i]/a[i][i]
+    for i in range(n-2,-1,-1):
+        x[i] = a[i][n]
+        for j in range(i+1,n):
+            x[i] = x[i] - a[i][j]*x[j]
+        
+        x[i] = x[i]/a[i][i]
 
-# Displaying all the solutions
-print('\nRequired solution is: ')
-for i in range(n):
-    print('X%d = %0.2f' %(i,x[i]), end = '\t')
+    # Displaying all the solutions
+    print('\nRequired solution is: ')
+    for i in range(n):
+        print('X%d = %0.2f' %(i,x[i]), end = '\t')
+
+Gauss()
