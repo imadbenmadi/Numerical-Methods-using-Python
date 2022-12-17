acf824e312bb5565dcc06f212d62cb2c89e8d7d2
imadbenmadi <106430872+imadbenmadi@users.noreply.github.com>
2022-12-17 21:30:48 +0100

    Update Gauss scidel.py

--- Gauss scidel.py
+++ Gauss scidel.py
@@ -1,10 +1,11 @@
 import numpy as np 
+from tabulate import tabulate
 
-def x_1(x2,x3): 
+def f1(x2,x3): 
     return (x2-2*x3+12)/5
-def x_2(x1,x3):                     
+def f2(x1,x3):                     
     return(-3*x1+2*x3-25)/8
-def x_3(x1,x2): 
+def f3(x1,x2): 
     return(-x1-x2+6)/4 
 
 x01=0
@@ -12,16 +13,18 @@ x02=0
 x03=0
 epsilon=0.001
 delta=True
+info = [["x01", "x02", "x03"]]
 while delta:
-    x1=x_1(x02,x03)
-    x2=x_2(x1,x03)
-    x3=x_3(x1,x2)
-    a = abs(x1-x01)
-    b = abs(x2-x02) 
-    c= abs(x3-x03)
+    x1=f1(x02,x03)
+    x2=f2(x1,x03)
+    x3=f3(x1,x2)
+    info.append([x1, x2, x3])
+    e1 = abs(x1-x01)
+    e2 = abs(x2-x02) 
+    e3 = abs(x3-x03)
     x01=x1
     x02=x2
     x03=x3
-    delta=a>epsilon and b>epsilon and c>epsilon
-
+    delta=e1>epsilon and e2>epsilon and e3>epsilon
 print('\n solution x1= ', round(x1,3),' x2= ',round(x2,3) ,' x3= ',round(x3,3))
+print(tabulate(info, headers='firstrow', tablefmt='fancy_grid'))
