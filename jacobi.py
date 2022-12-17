4bd06425f7fd64d786e837f51b2809ca8d590935
imadbenmadi <106430872+imadbenmadi@users.noreply.github.com>
2022-12-17 20:49:15 +0100

    Update jacobi.py

--- jacobi.py
+++ jacobi.py
@@ -18,8 +18,8 @@ def x3(x1, x2):
 #eps = [0.001, 0.001, 0.001]
 v = [np.array([1, 2, 3])]
 x = np.array([1, 2, 3])  # the initiale value of x
-
-for i in range(6):
+# 6 is the number of etirations i could not do it with 'while' 
+for i in range(6): 
     v.append([x1(x[1], x[2]), x2(x[0], x[2]), x3(x[0], x[1])])
     x = v[-1] #x = the last element of the v
     print("v =", v)
